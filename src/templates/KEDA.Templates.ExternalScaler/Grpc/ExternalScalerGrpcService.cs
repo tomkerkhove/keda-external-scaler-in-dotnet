de19d3cc9630faaeb3d5ff7d3e72bc85f5a2e3c4
Tom Kerkhove <kerkhove.tom@gmail.com>
2023-04-22 15:50:31 +0200

    Basic boilerplate
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/templates/KEDA.Templates.ExternalScaler/Grpc/ExternalScalerGrpcService.cs
@@ -0,0 +1,73 @@
+﻿using Generated.KEDA.Specs;
+using Grpc.Core;
+
+namespace KEDA.Templates.ExternalScaler.Grpc
+{
+    public class ExternalScalerGrpcService : Generated.KEDA.Specs.ExternalScaler.ExternalScalerBase
+    {
+        private const string MetricName = "<metric-name>";
+
+        public override async Task<IsActiveResponse> IsActive(ScaledObjectRef request, ServerCallContext context)
+        {
+            // request.Scalermetadata contain<s the `metadata` defined in the ScaledObject
+            if (!request.ScalerMetadata.ContainsKey("latitude") ||
+                !request.ScalerMetadata.ContainsKey("longitude"))
+            {
+                throw new ArgumentException("longitude and latitude must be specified");
+            }
+
+            return new IsActiveResponse
+            {
+                // return true if there is more than 2 Earthquakes with mag > 1.0
+                Result =true
+            };
+        }
+        
+        public override async Task StreamIsActive(ScaledObjectRef request,
+            IServerStreamWriter<IsActiveResponse> responseStream, ServerCallContext context)
+        {
+            if (!request.ScalerMetadata.ContainsKey("latitude") ||
+                !request.ScalerMetadata.ContainsKey("longitude"))
+            {
+                throw new ArgumentException("longitude and latitude must be specified");
+            }
+        
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                var response = new IsActiveResponse
+                {
+                    Result = true
+                };
+                await responseStream.WriteAsync(response);
+        
+                await Task.Delay(TimeSpan.FromHours(1));
+            }
+        }
+        
+        public override Task<GetMetricSpecResponse> GetMetricSpec(ScaledObjectRef request, ServerCallContext context)
+        {
+            var resp = new GetMetricSpecResponse();
+        
+            resp.MetricSpecs.Add(new MetricSpec
+            {
+                MetricName = MetricName,
+                TargetSize = 10
+            });
+        
+            return Task.FromResult(resp);
+        }
+
+        public override async Task<GetMetricsResponse> GetMetrics(GetMetricsRequest request, ServerCallContext context)
+        {
+        
+            var resp = new GetMetricsResponse();
+            resp.MetricValues.Add(new MetricValue
+            {
+                MetricName = MetricName,
+                MetricValue_ = 1
+            });
+        
+            return resp;
+        }
+    }
+}
---
+++ src/templates/KEDA.Templates.ExternalScaler/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace KEDA.Templates.ExternalScaler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .ConfigureKestrel(k => k.ListenAnyIP(5000, options =>
+                        {
+                            options.Protocols = HttpProtocols.Http2;
+                        }));
+                });
+    }
+}
